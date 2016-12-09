9a91b69592b495430b7c8974ac9420f61aa7bccf
WKD <wendy@posteo.de>
2016-12-09 18:23:41 -0100

    PickPattern.js wurde geändert

--- PickPattern.js
+++ PickPattern.js
@@ -1,3 +1,16 @@
+var chosenpat;
+var sizefile;
+
+document.getElementById("patterntable").addEventListener("change", choospattern);
+
+function choosepattern(){
+  var x = document.getElementById('patterntable').selectedIndex;
+  chosenpat = document.getElementById('patterntable').options[x].text;
+  selectmeasurementstoshow(chosenpat,sizefile);
+  showmeasurements();
+  changeBild(chosenpat);
+}
+
 function changeBild(chosenpat){
 switch (chosenpat){
 case "Bodice Women":
@@ -25,3 +38,32 @@ break;
 default:
 document.getElementById("Bild").src=document.Pattern.pattern.options[x].value;
 }}
+
+/*load patterns from a file*/
+function loadpatterns(file){
+$(document).ready(function(){
+    $.get(file,function(data){processData(data);
+    },"text");
+});}
+
+function processPatterns(allText) {
+    var allTextLines = allText.split(/\r\n|\n/);
+    var headers = allTextLines[0].split(',');
+    var lines = [];
+    emptytable();
+    for (var i=1; i<allTextLines.length; i++) {
+        var data = allTextLines[i].split(',');
+
+       newpatterntableitem(data[0],data[1]);
+    }}
+
+function newpatterntableitem(pattern, description){
+    /*
+    newpattern creates an entry in the pattern table in PickPattern
+    newpatterntableitem('name to be displayed', "description");
+    */
+    var patterntable=document.getElementById("pattern")
+    newitem = document.createElement("option");
+    newitem.innerHTML=pattern;
+    patterntable.appendChild(newitem);
+    };
